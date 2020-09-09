e24301948288ad852b172043395bf18089b63d99
cartario <cartario@yandex.ru>
2020-09-09 10:52:40 +0300

    Updates

---
+++ precache-manifest.630ae1e0881f8c38676ee560c28b0377.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ec1a5385d03b0836963220cb4273b0",
+    "url": "/moskom/index.html"
+  },
+  {
+    "revision": "ccbca2b045753f677448",
+    "url": "/moskom/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f6f6c43d6867907dd8bc",
+    "url": "/moskom/static/css/main.3d756c76.chunk.css"
+  },
+  {
+    "revision": "ccbca2b045753f677448",
+    "url": "/moskom/static/js/2.7c57043a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/moskom/static/js/2.7c57043a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6f6c43d6867907dd8bc",
+    "url": "/moskom/static/js/main.a790f5e2.chunk.js"
+  },
+  {
+    "revision": "e0d98ed1a29b585d428b",
+    "url": "/moskom/static/js/runtime-main.f2e16dfd.js"
+  }
+]);
